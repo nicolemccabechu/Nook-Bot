0a3ae02cfe88d552539308fe9ee8a43d277b2180
Nicole <nicolemccabechu@gmail.com>
2022-10-24 11:26:35 +0100

    Connecting MongoDB database

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const { token } = process.env;
+const { token, databaseToken } = process.env;
+const { connect } = require('mongoose');
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 
@@ -21,3 +22,6 @@ client.handleEvents();
 client.handleCommands();
 client.handleComponents();
 client.login(token);
+(async () => {
+  await connect(databaseToken).catch(console.error);
+})();
